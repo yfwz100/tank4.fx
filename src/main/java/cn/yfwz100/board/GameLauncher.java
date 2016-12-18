a31bed530108081e7c891963258e437ba1d3fae4
Zhi <yfwz100@yeah.net>
2016-12-18 23:05:01 +0800

    Make the app mac-ish and.

--- src/main/java/cn/yfwz100/board/GameLauncher.java
+++ src/main/java/cn/yfwz100/board/GameLauncher.java
@@ -1,7 +1,8 @@
 package cn.yfwz100.board;
 
+import de.codecentric.centerdevice.MenuToolkit;
 import javafx.application.Application;
-import javafx.fxml.FXML;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Label;
@@ -34,7 +35,6 @@ public class GameLauncher extends Application {
     @Override
     public void start(Stage theStage) {
         theStage.setTitle("Block Tank Game");
-        theStage.setAlwaysOnTop(true);
 
         BorderPane borderPane = new BorderPane();
         borderPane.setPrefSize(600, 400);
@@ -67,19 +67,19 @@ public class GameLauncher extends Application {
             MenuItem startMenuItem = new MenuItem("Start");
             startMenuItem.disableProperty().bind(gameLoop.activeProperty());
             startMenuItem.setAccelerator(KeyCombination.valueOf("meta+s"));
-            startMenuItem.setOnAction(e -> {
+            startMenuItem.setOnAction(e -> Platform.runLater(() -> {
                 borderPane.setCenter(gameCanvas);
                 gameLoop.start();
-            });
+            }));
             gameMenu.getItems().add(startMenuItem);
 
             MenuItem stopMenuItem = new MenuItem("Stop");
             stopMenuItem.disableProperty().bind(gameLoop.activeProperty().not());
             stopMenuItem.setAccelerator(KeyCombination.valueOf("meta+e"));
-            stopMenuItem.setOnAction(e -> {
+            stopMenuItem.setOnAction(e -> Platform.runLater(() -> {
                 borderPane.setCenter(scorePane);
                 gameLoop.stop();
-            });
+            }));
             gameMenu.getItems().add(stopMenuItem);
         }
 
@@ -90,13 +90,19 @@ public class GameLauncher extends Application {
             MenuItem resumeMenuItem = new MenuItem("Resume");
             resumeMenuItem.disableProperty().bind(gameLoop.activeProperty().not().or(gameLoop.runningProperty()));
             resumeMenuItem.setAccelerator(KeyCombination.valueOf("meta+r"));
-            resumeMenuItem.setOnAction(e -> gameLoop.resume());
+            resumeMenuItem.setOnAction(e -> Platform.runLater(() ->{
+                borderPane.setCenter(gameCanvas);
+                gameLoop.resume();
+            }));
             sessionMenu.getItems().add(resumeMenuItem);
 
             MenuItem pauseMenuItem = new MenuItem("Pause");
             pauseMenuItem.disableProperty().bind(gameLoop.activeProperty().not().or(gameLoop.runningProperty().not()));
             pauseMenuItem.setAccelerator(KeyCombination.valueOf("meta+p"));
-            pauseMenuItem.setOnAction(e -> gameLoop.pause());
+            pauseMenuItem.setOnAction(e -> Platform.runLater(() -> {
+                borderPane.setCenter(gameCanvas);
+                gameLoop.pause();
+            }));
             sessionMenu.getItems().add(pauseMenuItem);
         }
 
@@ -106,9 +112,19 @@ public class GameLauncher extends Application {
             helpMenu.getItems().add(howToMenuItem);
         }
 
+        // set the mac stuff.
+        MenuToolkit tk = MenuToolkit.toolkit();
+        Menu appMenu = tk.createDefaultApplicationMenu("TankII");
+        appMenu.getItems().get(0).setOnAction(e -> {
+            gameLoop.pause();
+            borderPane.setCenter(welcomePane);
+        });
+        tk.setApplicationMenu(appMenu);
+
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().addAll(gameMenu, sessionMenu, helpMenu);
         menuBar.setUseSystemMenuBar(true);
+
         borderPane.setTop(menuBar);
         //</editor-fold>
 
