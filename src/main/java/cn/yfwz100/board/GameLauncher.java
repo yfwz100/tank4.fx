17d11c257afc3028fa108ffc8346236fe44d0d8a
Zhi <yfwz100@yeah.net>
2016-12-19 22:22:23 +0800

    Add about pane and animations.

--- src/main/java/cn/yfwz100/board/GameLauncher.java
+++ src/main/java/cn/yfwz100/board/GameLauncher.java
@@ -1,20 +1,28 @@
 package cn.yfwz100.board;
 
 import de.codecentric.centerdevice.MenuToolkit;
+import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * The window of the board game.
@@ -32,33 +40,174 @@ public class GameLauncher extends Application {
         launch(args);
     }
 
+    /**
+     * The game canvas.
+     */
+    private Canvas gameCanvas;
+
+    /**
+     * The about pane.
+     */
+    private FlowPane aboutPane;
+
+    /**
+     * The score pane.
+     */
+    private FlowPane scorePane;
+
+    /**
+     * The game loop.
+     */
+    private GameLoop gameLoop;
+
     @Override
     public void start(Stage theStage) {
         theStage.setTitle("Block Tank Game");
 
-        BorderPane borderPane = new BorderPane();
-        borderPane.setPrefSize(600, 400);
-        Scene mainScene = new Scene(borderPane);
+        BorderPane mainPane = new BorderPane();
+        {
+            FadeTransition fadeTransition = new FadeTransition(Duration.seconds(5), mainPane);
+            fadeTransition.setFromValue(0);
+            fadeTransition.setToValue(1.0);
+            fadeTransition.play();
+        }
+        mainPane.setPrefSize(600, 400);
+        Scene mainScene = new Scene(mainPane);
         theStage.setScene(mainScene);
 
+        //<editor-fold defaultState="collapsed" desc="Init event handlers">
+        EventHandler<ActionEvent> startActionHandler = e -> Platform.runLater(() -> {
+            mainPane.setCenter(gameCanvas);
+
+            FadeTransition fadeTransition = new FadeTransition(Duration.millis(1000), aboutPane);
+            fadeTransition.setFromValue(0);
+            fadeTransition.setToValue(1.0);
+            fadeTransition.setCycleCount(1);
+            fadeTransition.setAutoReverse(false);
+            fadeTransition.setOnFinished(fe -> gameLoop.start());
+            fadeTransition.play();
+        });
+        EventHandler<ActionEvent> aboutActionHandler = e -> Platform.runLater(() -> {
+            mainPane.setCenter(aboutPane);
+
+            FadeTransition fadeTransition = new FadeTransition(Duration.millis(1000), aboutPane);
+            fadeTransition.setFromValue(0);
+            fadeTransition.setToValue(1.0);
+            fadeTransition.setCycleCount(1);
+            fadeTransition.setAutoReverse(false);
+            fadeTransition.play();
+        });
+        //</editor-fold>
+
         //<editor-fold defaultState="collapsed" desc="Construct the welcomePane.">
-        AnchorPane welcomePane = new AnchorPane();
-        ImageView bannerImage = new ImageView("cn/yfwz100/board/TankIIBanner.png");
-        AnchorPane.setTopAnchor(bannerImage, 200.0 - bannerImage.getImage().getHeight() / 2);
-        AnchorPane.setLeftAnchor(bannerImage, 300.0 - bannerImage.getImage().getWidth() / 2);
-        welcomePane.getChildren().add(bannerImage);
+        FlowPane welcomePane = new FlowPane();
+        {
+            welcomePane.setAlignment(Pos.CENTER);
+            welcomePane.setColumnHalignment(HPos.CENTER);
+            welcomePane.setOrientation(Orientation.VERTICAL);
+            welcomePane.setVgap(20);
+
+            ImageView bannerImage = new ImageView("cn/yfwz100/board/TankIIBanner.png");
+
+            Label versionLabel = new Label("Version: 2.0 (by yfwz100)");
+            versionLabel.setTextAlignment(TextAlignment.CENTER);
+
+            VBox buttonBar = new VBox();
+            Button startBtn = new Button("Start");
+            startBtn.setOnAction(startActionHandler);
+            startBtn.setPrefWidth(bannerImage.getImage().getWidth() * 0.5);
+
+            Button aboutBtn = new Button("About");
+            aboutBtn.setOnAction(aboutActionHandler);
+            aboutBtn.setPrefWidth(bannerImage.getImage().getWidth() * 0.5);
+
+            buttonBar.setAlignment(Pos.CENTER);
+            buttonBar.setSpacing(5);
+            buttonBar.getChildren().addAll(startBtn, aboutBtn);
+
+            welcomePane.getChildren().addAll(bannerImage, versionLabel, buttonBar);
+        }
         //</editor-fold>
 
         //<editor-fold defaultState="collapsed" desc="Construct the gameCanvas and gameLoop.">
-        Canvas gameCanvas = new Canvas(600, 400);
-        GameControl gameLoop = new GameLoop(gameCanvas);
+        gameCanvas = new Canvas(600, 400);
+        {
+            GraphicsContext g = gameCanvas.getGraphicsContext2D();
+            g.save();
+            g.setFill(Color.WHITE);
+            g.fillRect(0, 0, gameCanvas.getWidth(), gameCanvas.getHeight());
+            g.restore();
+        }
+        gameLoop = new GameLoop(gameCanvas);
+//        mainScene.setOnKeyPressed(e -> gameLoop.keyMapProperty().put(e.getCode(), true));
+//        mainScene.setOnKeyReleased(e -> gameLoop.keyMapProperty().put(e.getCode(), false));
         gameCanvas.setCache(true);
         //</editor-fold>
 
         //<editor-fold defaultState="collapsed" desc="Construct the scorePane.">
-        VBox scorePane = new VBox();
-        Label label = new Label("Score: 0");
-        scorePane.getChildren().add(label);
+        scorePane = new FlowPane();
+        {
+            scorePane.setAlignment(Pos.CENTER);
+            scorePane.setOrientation(Orientation.VERTICAL);
+            scorePane.setColumnHalignment(HPos.CENTER);
+            scorePane.setVgap(20);
+
+            ImageView logoView = new ImageView("cn/yfwz100/board/TankIIBanner.png");
+
+            GridPane detailBox = new GridPane();
+            {
+                detailBox.addRow(0, new Label("Achievement: "), styledLabel("Ordinary Hero!"));
+                detailBox.addRow(1, new Label("Score: "), styledLabel("1000"));
+                detailBox.addRow(2, new Label("Killed: "), styledLabel("10"));
+
+                ColumnConstraints headerConstraints = new ColumnConstraints();
+                headerConstraints.setHalignment(HPos.RIGHT);
+                ColumnConstraints valueConstraints = new ColumnConstraints();
+                valueConstraints.setHalignment(HPos.LEFT);
+                detailBox.getColumnConstraints().addAll(headerConstraints, valueConstraints);
+
+                RowConstraints rowConstraints = new RowConstraints();
+                rowConstraints.setValignment(VPos.BASELINE);
+                detailBox.getRowConstraints().addAll(rowConstraints, rowConstraints, rowConstraints);
+
+                detailBox.setVgap(5);
+                detailBox.setAlignment(Pos.CENTER);
+                detailBox.setPrefSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+            }
+
+            HBox buttonBar = new HBox();
+            Button shareBtn = new Button("↗ Share");
+            shareBtn.setDefaultButton(true);
+            Button restartBtn = new Button("↺ Restart");
+            restartBtn.setOnAction(startActionHandler);
+            buttonBar.setAlignment(Pos.CENTER);
+            buttonBar.setSpacing(10);
+            buttonBar.getChildren().addAll(shareBtn, restartBtn);
+
+            scorePane.getChildren().addAll(logoView, detailBox, buttonBar);
+        }
+        //</editor-fold>
+
+        //<editor-fold defaultState="" desc="Construct the aboutPane.">
+        aboutPane = new FlowPane();
+        {
+            ImageView bannerImage = new ImageView("cn/yfwz100/board/TankIIBanner.png");
+
+            Label descLabel = new Label("Press any key to continue.");
+            {
+                FadeTransition fadeTransition = new FadeTransition(Duration.millis(1000), descLabel);
+                fadeTransition.setFromValue(0.5);
+                fadeTransition.setToValue(1.0);
+                fadeTransition.setCycleCount(Timeline.INDEFINITE);
+                fadeTransition.setAutoReverse(true);
+                fadeTransition.play();
+            }
+
+            aboutPane.setOrientation(Orientation.VERTICAL);
+            aboutPane.setColumnHalignment(HPos.CENTER);
+            aboutPane.setAlignment(Pos.CENTER);
+            aboutPane.getChildren().addAll(bannerImage, descLabel);
+        }
         //</editor-fold>
 
         //<editor-fold defaultState="collapsed" desc="Construct main menus.">
@@ -67,17 +216,14 @@ public class GameLauncher extends Application {
             MenuItem startMenuItem = new MenuItem("Start");
             startMenuItem.disableProperty().bind(gameLoop.activeProperty());
             startMenuItem.setAccelerator(KeyCombination.valueOf("meta+s"));
-            startMenuItem.setOnAction(e -> Platform.runLater(() -> {
-                borderPane.setCenter(gameCanvas);
-                gameLoop.start();
-            }));
+            startMenuItem.setOnAction(startActionHandler);
             gameMenu.getItems().add(startMenuItem);
 
             MenuItem stopMenuItem = new MenuItem("Stop");
             stopMenuItem.disableProperty().bind(gameLoop.activeProperty().not());
             stopMenuItem.setAccelerator(KeyCombination.valueOf("meta+e"));
             stopMenuItem.setOnAction(e -> Platform.runLater(() -> {
-                borderPane.setCenter(scorePane);
+                mainPane.setCenter(scorePane);
                 gameLoop.stop();
             }));
             gameMenu.getItems().add(stopMenuItem);
@@ -90,8 +236,8 @@ public class GameLauncher extends Application {
             MenuItem resumeMenuItem = new MenuItem("Resume");
             resumeMenuItem.disableProperty().bind(gameLoop.activeProperty().not().or(gameLoop.runningProperty()));
             resumeMenuItem.setAccelerator(KeyCombination.valueOf("meta+r"));
-            resumeMenuItem.setOnAction(e -> Platform.runLater(() ->{
-                borderPane.setCenter(gameCanvas);
+            resumeMenuItem.setOnAction(e -> Platform.runLater(() -> {
+                mainPane.setCenter(gameCanvas);
                 gameLoop.resume();
             }));
             sessionMenu.getItems().add(resumeMenuItem);
@@ -100,7 +246,7 @@ public class GameLauncher extends Application {
             pauseMenuItem.disableProperty().bind(gameLoop.activeProperty().not().or(gameLoop.runningProperty().not()));
             pauseMenuItem.setAccelerator(KeyCombination.valueOf("meta+p"));
             pauseMenuItem.setOnAction(e -> Platform.runLater(() -> {
-                borderPane.setCenter(gameCanvas);
+                mainPane.setCenter(gameCanvas);
                 gameLoop.pause();
             }));
             sessionMenu.getItems().add(pauseMenuItem);
@@ -116,8 +262,11 @@ public class GameLauncher extends Application {
         MenuToolkit tk = MenuToolkit.toolkit();
         Menu appMenu = tk.createDefaultApplicationMenu("TankII");
         appMenu.getItems().get(0).setOnAction(e -> {
-            gameLoop.pause();
-            borderPane.setCenter(welcomePane);
+            if (gameLoop.isActive()) {
+                gameLoop.pause();
+            }
+            // delegate to the about action.
+            aboutActionHandler.handle(e);
         });
         tk.setApplicationMenu(appMenu);
 
@@ -125,10 +274,17 @@ public class GameLauncher extends Application {
         menuBar.getMenus().addAll(gameMenu, sessionMenu, helpMenu);
         menuBar.setUseSystemMenuBar(true);
 
-        borderPane.setTop(menuBar);
+        mainPane.setTop(menuBar);
         //</editor-fold>
 
-        borderPane.setCenter(welcomePane);
+        mainPane.setCenter(welcomePane);
         theStage.show();
     }
+
+    private static Label styledLabel(String text) {
+        Label label = new Label(text);
+        label.setFont(Font.font("HanziPen SC", Font.getDefault().getSize() + 2));
+        return label;
+    }
+
 }
