7ee03326bf673527a080b8378c2b868c94029733
Zhi <yfwz100@yeah.net>
2016-12-23 18:07:11 +0800

    Change the default level.

--- src/main/resources/cn/yfwz100/tank4/fx/battle/basic.js
+++ src/main/resources/cn/yfwz100/tank4/fx/battle/basic.js
@@ -6,19 +6,52 @@
 var StyledSimpleAITank = Packages.cn.yfwz100.tank4.fx.actor.StyledSimpleAITank;
 var StyledBlock = Packages.cn.yfwz100.tank4.fx.actor.StyledBlock;
 
-battle.getBlocks().add(new StyledBlock(battle,  60,  -1, 120,  1, 0));
-battle.getBlocks().add(new StyledBlock(battle, 121,  40,   1, 80, 0));
-battle.getBlocks().add(new StyledBlock(battle,  60,  81, 120,  1, 0));
-battle.getBlocks().add(new StyledBlock(battle,   -1,  40,   1, 80, 0));
+function block(x, y, w, h) {
+    x /= 5;
+    y /= 5;
+    w /= 5;
+    h /= 5;
+    return new StyledBlock(battle, x + w / 2, y + h / 2, w, h, 0);
+}
+
+function tank(x, y) {
+    return new StyledSimpleAITank(battle, x / 5, y / 5);
+}
+
+// the border of the battle.
+//battle.getBlocks().add(new StyledBlock(battle,  60,  -1, 120,  1, 0));
+//battle.getBlocks().add(new StyledBlock(battle, 121,  40,   1, 80, 0));
+//battle.getBlocks().add(new StyledBlock(battle,  60,  81, 120,  1, 0));
+//battle.getBlocks().add(new StyledBlock(battle,   -1,  40,   1, 80, 0));
+
+battle.getBlocks().add(block(0, -1, 600, 1, 0));
+battle.getBlocks().add(block(601, 0, 1, 400));
+battle.getBlocks().add(block(0, 400, 600, 1));
+battle.getBlocks().add(block(-1, 0, 1, 400));
 
 // initialize the tanks.
-battle.getTanks().add(new StyledSimpleAITank(battle,  10, 30));
-battle.getTanks().add(new StyledSimpleAITank(battle,  20, 45));
-battle.getTanks().add(new StyledSimpleAITank(battle,   3, 65));
-battle.getTanks().add(new StyledSimpleAITank(battle, 100, 70));
 
-// initialize the blocks.
-battle.getBlocks().add(new StyledBlock(battle, 0, 20, 30, 1, Math.PI / 18));
-battle.getBlocks().add(new StyledBlock(battle, 18, 40, 20, 1, -Math.PI / 18));
-battle.getBlocks().add(new StyledBlock(battle, 70, 60, 30, 1, 0));
-battle.getBlocks().add(new StyledBlock(battle, 50, 20, 20, 1, Math.PI / 2));
+//battle.getTanks().add(tank( 20, 45));
+//battle.getTanks().add(tank(  3, 65));
+//battle.getTanks().add(tank(100, 70));
+
+//function randomAddTank() {
+//    var x = math.random() * 600, y = math.random() * 400;
+//    battle.getTanks().add(tank(x, y));
+//}
+//setTimeout(randomAddTank, 5000);
+
+battle.getBlocks().add(block(100,  0, 10,100));
+battle.getBlocks().add(block(100,100, 90, 10));
+
+battle.getTanks().add(tank(500, 20));
+battle.getBlocks().add(block(500,  0, 10,100));
+battle.getBlocks().add(block(450,100, 90, 10));
+
+battle.getTanks().add(tank(300, 10));
+battle.getBlocks().add(block(  0,300,150, 10));
+battle.getBlocks().add(block(150,250, 10,100));
+
+for (var i=0; i<25; i++) {
+    battle.getTanks().add(tank(300 + (i%5) * 50, 200 + (i/5) * 50));
+}
